a81a4056675c85d6c7f5c41a22d1f9a36c49396a
jmarren <jmarren99@gmail.com>
2026-03-12 17:32:21 -0700

    appending middleware & nesters to structs

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/a-h/templ"
 	gorouter "github.com/jmarren/go-router"
@@ -35,6 +36,10 @@ func handleUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("users"))
 }
 
+func handleYellow(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("yellow"))
+}
+
 func handleJohn(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("john"))
 }
@@ -43,12 +48,50 @@ func isHxRequest(w http.ResponseWriter, r *http.Request) bool {
 	return r.Header.Get("HX-Request") != "true"
 }
 
+func NestBase(w http.ResponseWriter, r *http.Request, component templ.Component) templ.Component {
+	if isHxRequest(w, r) {
+		return views.Page(component)
+	}
+	return component
+}
+
 func handleHi(w http.ResponseWriter, r *http.Request) templ.Component {
 	return views.Hi()
 }
 
+func handleRed(w http.ResponseWriter, r *http.Request) templ.Component {
+	return views.Red()
+}
+
+func NestRed(w http.ResponseWriter, r *http.Request, component templ.Component) templ.Component {
+	if strings.Contains(r.URL.Path, "red") {
+		return views.RedNester(component)
+	}
+	return component
+}
+
 func main() {
 	app := gorouter.CreateApp()
+
+	app.UseNester(NestBase)
+	app.GetComponent("/", handleRoot)
+	app.GetComponent("/hi", handleHi)
+
+	subComponent := gorouter.CreateComponentRouter()
+
+	// subComponent := app.CreateSubRouter("/colors")
+
+	subComponent.UseNester(NestRed)
+
+	subComponent.Get("/yellow", handleYellow)
+	subComponent.GetComponent("/red", handleRed)
+	subComponent.GetComponent("/green", handleRed)
+
+	app.SubComponent("/colors", subComponent)
+
+	// app.SubRoute("/red", subComponent)
+
+	// app.SubRoute("/red", subComponent)
 
 	// defines the outer component
 	// app.UsePage(views.Base)
@@ -60,15 +103,15 @@ func main() {
 	// basePage.Get("/home", handlers.Home)
 	// basePage
 
-	base := gorouter.CreateNestedComponentRouter(views.Page, isHxRequest)
-
-	base.Get("/home", handleRoot)
-
-	base.Component("/hi", handleHi)
-
+	// base := gorouter.CreateNestedComponentRouter(views.Page, isHxRequest)
+	//
+	// base.Get("/home", handleRoot)
+	//
+	// base.Component("/hi", handleHi)
+	//
 	// base.Component("/hi",
 
-	app.Page("/", "GET", handleRoot)
+	// app.Page("/", "GET", handleRoot)
 	app.Use(loggerOne)
 	app.Use(loggerTwo)
 	app.Get("/log", handleLog)
@@ -76,9 +119,9 @@ func main() {
 	usersRouter.Get("", handleUsers)
 	johnRouter := gorouter.CreateRouter()
 	johnRouter.Get("", handleJohn)
-	usersRouter.SubRoute("/john", johnRouter)
-	app.SubRoute("/users", usersRouter)
-	app.Component("/johno", handleRoot).Get()
+	// usersRouter.SubRoute("/john", johnRouter)
+	// app.SubRoute("/users", usersRouter)
+	// app.Component("/johno", handleRoot).Get()
 
 	app.Serve(":6060")
 
--- router.go
+++ router.go
@@ -4,133 +4,124 @@ import (
 	"net/http"
 
 	"github.com/a-h/templ"
-	"github.com/jmarren/go-router/views"
 )
 
 type Middleware func(h func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request)
 
 type Route struct {
-	path    string
-	method  string
-	handler func(w http.ResponseWriter, r *http.Request)
+	path        string
+	method      string
+	middlewares []Middleware
+	handler     func(w http.ResponseWriter, r *http.Request)
+}
+
+func (route *Route) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for i := 0; i < len(route.middlewares); i++ {
+		route.handler = route.middlewares[i](route.handler)
+	}
+	route.handler(w, r)
+}
+
+type ComponentRoute struct {
+	middlewares []Middleware
+	nesters     []Nester
+	path        string
+	method      string
+	handler     func(w http.ResponseWriter, r *http.Request) templ.Component
+}
+
+func (c *ComponentRoute) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	handler := func(w http.ResponseWriter, r *http.Request) {}
+
+	// apply middlewares
+	for i := 0; i < len(c.middlewares); i++ {
+		handler = c.middlewares[i](handler)
+	}
+
+	component := c.handler(w, r)
+
+	// apply nesters
+	for i := 0; i < len(c.nesters); i++ {
+		component = c.nesters[i](w, r, component)
+	}
+
+	component.Render(r.Context(), w)
 }
 
 type Router struct {
 	middlewares []Middleware
-	routes      []Route
+	routes      []*Route
 }
 
-type Nester func(w http.ResponseWriter, r *http.Request) bool
+type Nester func(w http.ResponseWriter, r *http.Request, component templ.Component) templ.Component
 
 type ComponentRouter struct {
 	*Router
-	base   BaseComponent
-	nester Nester
+	componentRoutes []*ComponentRoute
+	nesters         []Nester
 }
-
-type BaseComponent func(base templ.Component) templ.Component
 
 type App struct {
 	mux *http.ServeMux
-	// *Router
 	*ComponentRouter
-	// base BaseComponent
-}
-
-func (c *ComponentRouter) UseBase(base BaseComponent) {
-	c.base = base
 }
 
 func (c *ComponentRouter) UseNester(n Nester) {
-	c.nester = n
+	c.nesters = append([]Nester{n}, c.nesters...)
 }
 
 type ComponentHandler func(w http.ResponseWriter, r *http.Request) templ.Component
 
-func (c *ComponentRouter) nest(ch ComponentHandler) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		component := ch(w, r)
-
-		// if nester returns true
-		// wrap the component with base
-		if c.nester(w, r) {
-			component = c.base(component)
-		}
-		component.Render(r.Context(), w)
-	}
-
-}
 func (c *ComponentRouter) GetComponent(path string, ch ComponentHandler) {
-	c.Get(path, c.nest(ch))
+	c.componentRoutes = append(c.componentRoutes, &ComponentRoute{
+		nesters:     c.nesters,
+		path:        path,
+		method:      "GET",
+		handler:     ch,
+		middlewares: c.middlewares,
+	})
 }
 
 func (c *ComponentRouter) PostComponent(path string, ch ComponentHandler) {
-	c.Post(path, c.nest(ch))
+	c.componentRoutes = append(c.componentRoutes, &ComponentRoute{
+		nesters:     c.nesters,
+		path:        path,
+		method:      "POST",
+		handler:     ch,
+		middlewares: c.middlewares,
+	})
 }
 
 func (c *ComponentRouter) PutComponent(path string, ch ComponentHandler) {
-	c.Put(path, c.nest(ch))
+	c.componentRoutes = append(c.componentRoutes, &ComponentRoute{
+		nesters:     c.nesters,
+		path:        path,
+		method:      "PUT",
+		handler:     ch,
+		middlewares: c.middlewares,
+	})
 }
 
 func (c *ComponentRouter) DeleteComponent(path string, ch ComponentHandler) {
-	c.Delete(path, c.nest(ch))
+	c.componentRoutes = append(c.componentRoutes, &ComponentRoute{
+		path:        path,
+		nesters:     c.nesters,
+		method:      "DELETE",
+		handler:     ch,
+		middlewares: c.middlewares,
+	})
 }
-
-// func (a *App)
-//
-// type ShouldRenderFull func(w http.ResponseWriter, r *http.Request) bool
-//
-// type NestedComponentRouter struct {
-// 	base             BaseComponent
-// 	shouldRenderFull ShouldRenderFull
-// 	*Router
-// }
-//
-// func CreateNestedComponentRouter(baseFn func(base templ.Component) templ.Component, shouldRenderFull ShouldRenderFull) *NestedComponentRouter {
-// 	return &NestedComponentRouter{
-// 		base:             baseFn,
-// 		Router:           CreateRouter(),
-// 		shouldRenderFull: shouldRenderFull,
-// 	}
-// }
-//
-// func (n *NestedComponentRouter) handler(ch func(w http.ResponseWriter, r *http.Request) templ.Component) func(w http.ResponseWriter, r *http.Request) {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		component := ch(w, r)
-// 		if n.shouldRenderFull(w, r) {
-// 			component = n.base(component)
-// 		}
-// 		component.Render(r.Context(), w)
-// 	}
-// }
-//
-// func (n *NestedComponentRouter) Get(path string, ch func(w http.ResponseWriter, r *http.Request) templ.Component) {
-// 	n.Router.Get(path, n.handler(ch))
-// }
-
-//	type ComponentRouter struct {
-//		Get    func()
-//		Post   func()
-//		Put    func()
-//		Delete func()
-//	}
-
 func CreateRouter() *Router {
 	return &Router{
 		middlewares: []Middleware{},
-		routes:      []Route{},
+		routes:      []*Route{},
 	}
 }
 
 func CreateComponentRouter() *ComponentRouter {
 	return &ComponentRouter{
-		Router: CreateRouter(),
-		base: func(base templ.Component) templ.Component {
-			return base
-		},
-		nester: func(w http.ResponseWriter, r *http.Request) bool {
-			return true
-		},
+		Router:  CreateRouter(),
+		nesters: []Nester{},
 	}
 }
 
@@ -146,84 +137,52 @@ func (r *Router) Use(m Middleware) {
 	r.middlewares = append(r.middlewares, m)
 }
 
-// applies a routers middleware to the provided handler
-func (r *Router) applyMiddlewares(handler func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
-	for i := len(r.middlewares) - 1; i >= 0; i-- {
-		handler = r.middlewares[i](handler)
-	}
-	return handler
-}
-
 // registers a get route with the router
 func (r *Router) Get(path string, handler func(w http.ResponseWriter, r *http.Request)) {
-	r.routes = append(r.routes, Route{
-		path:    path,
-		method:  "GET",
-		handler: r.applyMiddlewares(handler),
+	r.routes = append(r.routes, &Route{
+		path:        path,
+		method:      "GET",
+		handler:     handler,
+		middlewares: r.middlewares,
 	})
 }
 
 // registers a post route with the router
 func (r *Router) Post(path string, handler http.HandlerFunc) {
-	r.routes = append(r.routes, Route{
-		path:    path,
-		method:  "POST",
-		handler: r.applyMiddlewares(handler),
+	r.routes = append(r.routes, &Route{
+		path:        path,
+		method:      "POST",
+		handler:     handler,
+		middlewares: r.middlewares,
 	})
 }
 
 // registers a put route with the router
 func (r *Router) Put(path string, handler http.HandlerFunc) {
-	r.routes = append(r.routes, Route{
-		path:    path,
-		method:  "PUT",
-		handler: r.applyMiddlewares(handler),
+	r.routes = append(r.routes, &Route{
+		path:        path,
+		method:      "PUT",
+		handler:     handler,
+		middlewares: r.middlewares,
 	})
 }
 
 // registers a delete route with the router
 func (r *Router) Delete(path string, handler http.HandlerFunc) {
-	r.routes = append(r.routes, Route{
-		path:    path,
-		method:  "DELETE",
-		handler: r.applyMiddlewares(handler),
+	r.routes = append(r.routes, &Route{
+		path:        path,
+		method:      "DELETE",
+		handler:     handler,
+		middlewares: r.middlewares,
 	})
 }
 
-// renders a component for the given get request path
-// func (r *Router) Component(path string, ch func(w http.ResponseWriter, r *http.Request) templ.Component) *ComponentRouter {
-//
-//		handler := func(w http.ResponseWriter, r *http.Request) {
-//			component := ch(w, r)
-//			component.Render(r.Context(), w)
-//		}
-//		return &ComponentRouter{
-//			Get: func() {
-//				r.Get(path, handler)
-//			},
-//			Post: func() {
-//				r.Post(path, handler)
-//			},
-//			Put: func() {
-//				r.Put(path, handler)
-//			},
-//			Delete: func() {
-//				r.Delete(path, handler)
-//			},
-//		}
-//	}
-func (r *Router) Page(path string, method string, ch func(w http.ResponseWriter, r *http.Request) templ.Component) {
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		component := ch(w, r)
-		// if hx-request render only the component
-		// otherwise render inside the root page component
-		if r.Header.Get("HX-Request") == "true" {
-			component.Render(r.Context(), w)
-		} else {
-			views.Page(component).Render(r.Context(), w)
-		}
-	}
-	r.Get(path, handler)
+type IRouter interface {
+	Routes() []Route
+}
+
+func (r *Router) Routes() []*Route {
+	return r.routes
 }
 
 /*
@@ -233,19 +192,42 @@ This includes concatenating the routes path and applying the routers middleware
 to the routes handler
 */
 func (r *Router) SubRoute(path string, subRoute *Router) {
+
 	for _, route := range subRoute.routes {
-		r.routes = append(r.routes, Route{
-			path:    path + route.path,
-			method:  route.method,
-			handler: r.applyMiddlewares(route.handler),
+		r.routes = append(r.routes, &Route{
+			path:        path + route.path,
+			method:      route.method,
+			handler:     route.handler,
+			middlewares: append(route.middlewares, r.middlewares...),
 		})
 	}
 }
 
+func (c *ComponentRouter) SubComponent(path string, subComponent *ComponentRouter) {
+	for _, cr := range subComponent.componentRoutes {
+		c.componentRoutes = append(c.componentRoutes, &ComponentRoute{
+			path:        path + cr.path,
+			method:      cr.method,
+			handler:     cr.handler,
+			nesters:     append(cr.nesters, c.nesters...),
+			middlewares: append(cr.middlewares, c.middlewares...),
+		})
+	}
+
+	c.SubRoute(path, subComponent.Router)
+}
+
+// func serveComponent(w http.Response
+
 // applies the apps routes to the apps mux
 func (a *App) applyRoutes() {
+
 	for _, route := range a.routes {
-		a.mux.Handle(route.method+" "+route.path, http.HandlerFunc(route.handler))
+		a.mux.Handle(route.path, route)
+	}
+
+	for _, route := range a.componentRoutes {
+		a.mux.Handle(route.path, route)
 	}
 }
 
